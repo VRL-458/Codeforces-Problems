d2681d3ee1eb52ad7ca7517026c736d35337e17f
Leonardo Ampuero <leonardo.ampuero.terceros@gmail.com>
2024-04-15 18:11:53 -0400

    some files solved and .gitignore for .exe added

---
+++ Domini Piling/Domoini Piling.cpp
@@ -0,0 +1,14 @@
+#include <vector>
+#include <string>
+#include <iostream>
+
+using namespace std;
+
+int main()
+{       
+    int m, n;
+    cin >> m>>n;
+    cout <<m*n / 2;
+        
+    return 0;
+}
